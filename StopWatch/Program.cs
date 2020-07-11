44f63484e989e1c1153ba7027e5dca21f7f99e21
jdenny95 <51931801+jdenny95@users.noreply.github.com>
2020-07-11 23:21:24 +0100

    Add files via upload

---
+++ StopWatch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To start the stopwatch, type 'start'. To stop the stopwatch, type 'stop'. When you want to exit the application, type 'exit'.");
+            var stopwatch = new Stopwatch();
+
+            while (true)
+            {
+                try
+                {
+                    var input = Console.ReadLine();
+
+                    switch (input.ToLower())
+                    {
+                        case "start":
+                            stopwatch.Start();
+                            break;
+                        case "stop":
+                            stopwatch.Stop();
+                            Console.WriteLine(stopwatch.Duration);
+                            break;
+                        case "exit":
+                            stopwatch.Exit();
+                            break;
+                        default:
+                            Console.WriteLine("Unexpect input. Please type 'start' to start the stopwatch, 'stop' to stop it or 'exit' to exit the application.");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ StopWatch/Stopwatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class Stopwatch
+    {
+        private DateTime _start;
+        public TimeSpan Duration { get; private set; }
+        public bool RunningStatus { get; private set; }
+
+        public void Start()
+        {
+            if (RunningStatus)
+                throw new InvalidOperationException("You have already started the stopwatch.");
+
+            _start = DateTime.UtcNow;
+            RunningStatus = true;
+        }
+
+        public void Stop()
+        {
+            if(!RunningStatus)
+                throw new InvalidOperationException("You have not started the stopwatch yet.");
+
+            RunningStatus = false;
+            Duration = DateTime.UtcNow - _start;
+        }
+
+        public void Exit()
+        {
+            Environment.Exit(0);            
+        }
+    }
+}
